9a73c0819a390666d691c599c1cb910add84a1ef
SP-hash <amigaguru@gmail.com>
2018-09-10 07:30:14 +0200

    fix sleep

--- ccminer.cpp
+++ ccminer.cpp
@@ -1849,7 +1849,7 @@ static void *miner_thread(void *userdata)
 				work_done = true; // force "regen" hash
 			while (!work_done && time(NULL) >= (g_work_time + opt_scantime)) 
 			{
-				Sleep(1);
+				uleep(1);
 				if (sleeptime > 4) {
 					extrajob = true;
 					break;
