7803ba8157226e19d6301743b71db5a002de2ea7
SP-hash <amigaguru@gmail.com>
2019-09-13 21:55:14 +0200

    fix

--- ccminer.cpp
+++ ccminer.cpp
@@ -3139,6 +3139,7 @@ void parse_arg(int key, char *arg)
 		if (v < 1 || v > 65535) // sanity check
 			show_usage_and_exit(1);
 		opt_api_mcast_port = v;
+		break;
 	case 'B':
 		opt_background = true;
 		break;
